5c4a43f965dfc562353b801f11e2c217cb82f5af
kcuevas99 <k3cuevas@protonmail.com>
2021-11-28 18:37:42 -0400

    agregados los ultimos cambios para pase a produccion

--- src/main/java/com/kencuevas/shoppingsystem/ShoppingSystemApplication.java
+++ src/main/java/com/kencuevas/shoppingsystem/ShoppingSystemApplication.java
@@ -1,13 +1,17 @@
 package com.kencuevas.shoppingsystem;
 
+import com.kencuevas.shoppingsystem.models.Role;
+import com.kencuevas.shoppingsystem.repositories.RoleRepository;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class ShoppingSystemApplication {
+public class ShoppingSystemApplication  implements CommandLineRunner {
 
     @Bean
     public ModelMapper modelMapper(){
@@ -18,4 +22,17 @@ public class ShoppingSystemApplication {
         SpringApplication.run(ShoppingSystemApplication.class, args);
     }
 
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        Role adminRole = new Role();
+        adminRole.setName("ROLE_ADMIN");
+        roleRepository.save(adminRole);
+
+        Role userRole = new Role();
+        userRole.setName("ROLE_USER");
+        roleRepository.save(userRole);
+    }
 }
